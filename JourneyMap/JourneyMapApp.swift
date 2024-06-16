11109b0c988cd8086bfc5063917da0321009d5b5
aicccux <72127637+aicccux@users.noreply.github.com>
2024-06-16 19:57:07 +0900

    Update JourneyMapApp.swift

--- JourneyMap/JourneyMapApp.swift
+++ JourneyMap/JourneyMapApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct JourneyMapApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            MarkerItem.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
